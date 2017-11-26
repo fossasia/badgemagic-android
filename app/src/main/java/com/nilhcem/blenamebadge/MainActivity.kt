be11b8370d8cd1d82b99d57611337b438636b494
Gautier Nilhcem <nilhcem@gmail.com>
2017-11-26 11:07:31 +0100

    Add simple layout

--- app/src/main/java/com/nilhcem/blenamebadge/MainActivity.kt
+++ app/src/main/java/com/nilhcem/blenamebadge/MainActivity.kt
@@ -2,11 +2,22 @@ package com.nilhcem.blenamebadge
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
+import com.nilhcem.blenamebadge.core.android.log.Timber
+import com.nilhcem.blenamebadge.core.android.viewbinding.bindView
 
 class MainActivity : AppCompatActivity() {
 
+    val content: EditText by bindView(R.id.text_to_send)
+    val send: Button by bindView(R.id.send_button)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_activity)
+
+        send.setOnClickListener {
+            Timber.i { "Text to send: ${content.text.trim()}" }
+        }
     }
 }
