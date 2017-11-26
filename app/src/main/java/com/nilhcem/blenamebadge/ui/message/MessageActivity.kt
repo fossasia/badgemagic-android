81b2bbf15e14a735fc95ba4a07c277ca721f7aec
Gautier Nilhcem <nilhcem@gmail.com>
2017-11-26 12:08:38 +0100

    Start creating MessagePresenter

--- app/src/main/java/com/nilhcem/blenamebadge/ui/message/MessageActivity.kt
+++ app/src/main/java/com/nilhcem/blenamebadge/ui/message/MessageActivity.kt
@@ -6,20 +6,23 @@ import android.widget.Button
 import android.widget.EditText
 import com.nilhcem.blenamebadge.R
 import com.nilhcem.blenamebadge.core.android.ext.showKeyboard
-import com.nilhcem.blenamebadge.core.android.log.Timber
 import com.nilhcem.blenamebadge.core.android.viewbinding.bindView
+import com.nilhcem.blenamebadge.device.model.DataToSend
+import com.nilhcem.blenamebadge.device.model.Message
 
 class MessageActivity : AppCompatActivity() {
 
     private val content: EditText by bindView(R.id.text_to_send)
     private val send: Button by bindView(R.id.send_button)
 
+    private val presenter by lazy { MessagePresenter() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.message_activity)
 
         send.setOnClickListener {
-            Timber.i { "Text to send: ${content.text.trim()}" }
+            presenter.sendMessage(convertToDeviceDataModel())
         }
     }
 
@@ -27,5 +30,9 @@ class MessageActivity : AppCompatActivity() {
         super.onResume()
         content.requestFocus()
         content.showKeyboard()
+    }
+
+    private fun convertToDeviceDataModel(): DataToSend {
+        return DataToSend(listOf(Message(content.text.trim().toString())))
     }
 }
---
+++ app/src/main/java/com/nilhcem/blenamebadge/ui/message/MessagePresenter.kt
@@ -0,0 +1,13 @@
+package com.nilhcem.blenamebadge.ui.message
+
+import com.nilhcem.blenamebadge.core.android.log.Timber
+import com.nilhcem.blenamebadge.device.DataToByteArrayConverter
+import com.nilhcem.blenamebadge.device.model.DataToSend
+
+class MessagePresenter {
+
+    fun sendMessage(dataToSend: DataToSend) {
+        Timber.i { "About to send data: $dataToSend" }
+        val byteData = DataToByteArrayConverter.convert(dataToSend)
+    }
+}
