d241385ebdc46c7dc567df9e8358141238be3fb1
Gautier Nilhcem <nilhcem@gmail.com>
2017-11-26 23:43:24 +0100

    Refactor unit test

--- app/src/test/java/com/nilhcem/blenamebadge/device/DataToByteArrayConverterTest.kt
+++ app/src/test/java/com/nilhcem/blenamebadge/device/DataToByteArrayConverterTest.kt
@@ -2,7 +2,7 @@ package com.nilhcem.blenamebadge.device
 
 import com.nilhcem.blenamebadge.device.model.DataToSend
 import com.nilhcem.blenamebadge.device.model.Message
-import org.amshove.kluent.`should be equal to`
+import org.amshove.kluent.`should equal`
 import org.amshove.kluent.`should throw`
 import org.junit.Test
 
@@ -21,17 +21,21 @@ class DataToByteArrayConverterTest {
     }
 
     @Test
-    fun `result should start with 77616E67`() {
+    fun `result should start with 77616E67000000`() {
         // Given
         val data = DataToSend(listOf(Message("A")))
 
         // When
-        val result = DataToByteArrayConverter.convert(data).first()
+        val result = DataToByteArrayConverter.convert(data).join()
 
         // Then
-        result[0] `should be equal to` 0x77
-        result[1] `should be equal to` 0x61
-        result[2] `should be equal to` 0x6E
-        result[3] `should be equal to` 0x67
+        val expected: List<Byte> = listOf(0x77, 0x61, 0x6E, 0x67, 0x00, 0x00)
+        result.take(6) `should equal` expected
+    }
+
+    private fun List<ByteArray>.join(): ByteArray {
+        var byteArray = ByteArray(0)
+        forEach { byteArray = byteArray.plus(it) }
+        return byteArray
     }
 }
