80ee86576703f72bea732a996df2e079e4259112
Gautier Nilhcem <nilhcem@gmail.com>
2017-11-26 11:53:46 +0100

    Rename MainActivity -> MessageActivity

--- app/src/main/java/com/nilhcem/blenamebadge/ui/message/MessageActivity.kt
+++ app/src/main/java/com/nilhcem/blenamebadge/ui/message/MessageActivity.kt
@@ -1,21 +1,22 @@
-package com.nilhcem.blenamebadge
+package com.nilhcem.blenamebadge.ui.message
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.EditText
+import com.nilhcem.blenamebadge.R
 import com.nilhcem.blenamebadge.core.android.ext.showKeyboard
 import com.nilhcem.blenamebadge.core.android.log.Timber
 import com.nilhcem.blenamebadge.core.android.viewbinding.bindView
 
-class MainActivity : AppCompatActivity() {
+class MessageActivity : AppCompatActivity() {
 
     private val content: EditText by bindView(R.id.text_to_send)
     private val send: Button by bindView(R.id.send_button)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        setContentView(R.layout.message_activity)
 
         send.setOnClickListener {
             Timber.i { "Text to send: ${content.text.trim()}" }
