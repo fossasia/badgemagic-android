3987829d21bbf35cd8b4cafdcc21806a643e426b
Gautier Nilhcem <nilhcem@gmail.com>
2017-11-28 23:55:43 +0100

    Send data via Bluetooth

--- app/src/main/java/com/nilhcem/blenamebadge/ui/message/MessageActivity.kt
+++ app/src/main/java/com/nilhcem/blenamebadge/ui/message/MessageActivity.kt
@@ -1,16 +1,32 @@
 package com.nilhcem.blenamebadge.ui.message
 
+import android.Manifest
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.nilhcem.blenamebadge.R
 import com.nilhcem.blenamebadge.core.android.ext.showKeyboard
+import com.nilhcem.blenamebadge.core.android.log.Timber
 import com.nilhcem.blenamebadge.core.android.viewbinding.bindView
 import com.nilhcem.blenamebadge.device.model.DataToSend
 import com.nilhcem.blenamebadge.device.model.Message
 
 class MessageActivity : AppCompatActivity() {
+
+    companion object {
+        private const val REQUEST_ENABLE_BT = 1
+        private const val REQUEST_PERMISSION_LOCATION = 1
+    }
 
     private val content: EditText by bindView(R.id.text_to_send)
     private val send: Button by bindView(R.id.send_button)
@@ -22,17 +38,71 @@ class MessageActivity : AppCompatActivity() {
         setContentView(R.layout.message_activity)
 
         send.setOnClickListener {
-            presenter.sendMessage(convertToDeviceDataModel())
+            presenter.sendMessage(this, convertToDeviceDataModel())
         }
     }
 
     override fun onResume() {
         super.onResume()
+        prepareForScan()
         content.requestFocus()
         content.showKeyboard()
     }
 
+    override fun onPause() {
+        super.onPause()
+        presenter.onPause()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_CANCELED) {
+            Toast.makeText(this, R.string.enable_bluetooth, Toast.LENGTH_LONG).show()
+            finish()
+            return
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_PERMISSION_LOCATION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Timber.d { "Location permission accepted" }
+            } else {
+                Toast.makeText(this, R.string.grant_location_permission, Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
     private fun convertToDeviceDataModel(): DataToSend {
         return DataToSend(listOf(Message(content.text.trim().toString())))
+    }
+
+    private fun prepareForScan() {
+        if (isBleSupported()) {
+            // Ensures Bluetooth is enabled on the device
+            val btManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+            val btAdapter = btManager.adapter
+            if (btAdapter.isEnabled) {
+                // Prompt for runtime permission
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    Timber.i { "Coarse permission granted" }
+                } else {
+                    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), REQUEST_PERMISSION_LOCATION)
+                }
+            } else {
+                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+            }
+        } else {
+            Toast.makeText(this, "BLE is not supported", Toast.LENGTH_LONG).show()
+            finish()
+        }
+    }
+
+    private fun isBleSupported(): Boolean {
+        return packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)
     }
 }
