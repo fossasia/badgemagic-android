0870e3b011955a6c1efbbebec254ca2ee9573f48
Travis CI <noreply@travis.com>
2018-12-15 16:43:15 +0000

    Travis build pushed [development]

--- app/src/main/java/com/nilhcem/blenamebadge/device/bluetooth/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.nilhcem.blenamebadge.device.bluetooth
-
-import java.util.*
-
-object Constants {
-    val SERVICE_UUID: UUID = UUID.fromString("0000fee0-0000-1000-8000-00805f9b34fb")
-    val CHARACTERISTIC_UUID: UUID = UUID.fromString("0000fee1-0000-1000-8000-00805f9b34fb")
-}
