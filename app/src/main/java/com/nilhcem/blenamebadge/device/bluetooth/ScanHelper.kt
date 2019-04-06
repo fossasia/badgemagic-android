9887721c3c5691513264d4b6e4d797a877947951
thelimitbreaker <rohanmaityofficial@gmail.com>
2019-04-06 14:05:57 +0530

    fix scanLogic

--- app/src/main/java/com/nilhcem/blenamebadge/device/bluetooth/ScanHelper.kt
+++ app/src/main/java/com/nilhcem/blenamebadge/device/bluetooth/ScanHelper.kt
@@ -43,7 +43,7 @@ class ScanHelper {
     }
 
     fun startLeScan(onDeviceFoundCallback: ((BluetoothDevice?) -> Unit)) {
-        if (!isScanning) return
+        if (isScanning) return
         this.onDeviceFoundCallback = onDeviceFoundCallback
         isScanning = true
 
