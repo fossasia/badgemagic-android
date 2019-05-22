898b5568607bdc9b6ab7c911464dae0b8f27ef44
adityastic <adityaofficialgupta@gmail.com>
2019-05-22 11:57:32 +0530

    fix: converted java file to kotlin

--- app/src/main/java/org/fossasia/badgemagic/ui/custom/knob/OnCrollerChangeListener.java
+++
@@ -1,9 +0,0 @@
-package org.fossasia.badgemagic.ui.custom.knob;
-
-public interface OnCrollerChangeListener {
-    void onProgressChanged(Croller croller, int progress);
-
-    void onStartTrackingTouch(Croller croller);
-
-    void onStopTrackingTouch(Croller croller);
-}
