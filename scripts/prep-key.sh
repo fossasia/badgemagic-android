fe7c9f89e854b1ee2d8509ba7da0d98f222c759d
adityastic <adityaofficialgupta@gmail.com>
2019-05-23 06:18:59 +0530

    feat: automatic release

--- scripts/prep-key.sh
+++ scripts/prep-key.sh
@@ -3,11 +3,10 @@ set -e
 
 export DEPLOY_BRANCH=${DEPLOY_BRANCH:-master}
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "fossasia/badge-magic-android" -o "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
+if [[ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "fossasia/badge-magic-android" -o "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]]; then
     echo "We decrypt key only for pushes to the master branch and not PRs. So, skip."
     exit 0
 fi
 
-# TODO: Settle signing keys
-# openssl aes-256-cbc -K $encrypted_d4de000c59f7_key -iv $encrypted_d4de000c59f7_iv -in ./scripts/secrets.tar.enc -out ./scripts/secrets.tar -d
-# tar xvf ./scripts/secrets.tar -C scripts/
+openssl aes-256-cbc -K $encrypted_f10b5e0e5262_key -iv $encrypted_f10b5e0e5262_iv -in ./scripts/secrets.tar.enc -out ./scripts/secrets.tar -d
+tar xvf ./scripts/secrets.tar -C scripts/
