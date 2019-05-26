d42ecbd931f591b5bea9adebe622bba38e93d682
adityastic <adityaofficialgupta@gmail.com>
2019-05-26 21:19:57 +0530

    chore: Updated Conditions in scripts

--- scripts/prep-key.sh
+++ scripts/prep-key.sh
@@ -3,7 +3,7 @@ set -e
 
 export DEPLOY_BRANCH=${DEPLOY_BRANCH:-master}
 
-if [[ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "fossasia/badge-magic-android" -o "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "fossasia/badge-magic-android" -o "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
     echo "We decrypt key only for pushes to the master branch and not PRs. So, skip."
     exit 0
 fi
