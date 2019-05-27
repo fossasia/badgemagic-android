a33c8ff1965b1eebd4d32ed9076e75893fd69a75
adityastic <adityaofficialgupta@gmail.com>
2019-05-27 06:31:48 +0530

    chore: updated getDrawable deprecation

--- app/src/main/java/org/fossasia/badgemagic/adapter/DrawableItemHolder.kt
+++ app/src/main/java/org/fossasia/badgemagic/adapter/DrawableItemHolder.kt
@@ -1,6 +1,5 @@
 package org.fossasia.badgemagic.adapter
 
-import android.graphics.Color
 import android.graphics.drawable.BitmapDrawable
 import android.view.View
 import android.widget.ImageView
@@ -9,20 +8,16 @@ import androidx.recyclerview.widget.RecyclerView
 import org.fossasia.badgemagic.R
 import org.fossasia.badgemagic.data.DrawableInfo
 import org.fossasia.badgemagic.util.ImageUtils
-import org.fossasia.badgemagic.util.Resource
-
 class DrawableItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     private val card: LinearLayout = itemView.findViewById(R.id.card)
     private val image: ImageView = itemView.findViewById(R.id.image)
     var listener: OnDrawableSelected? = null
-    private val resource = Resource()
 
     fun bind(drawableInfo: DrawableInfo) {
         image.setImageBitmap(ImageUtils.trim((drawableInfo.image as BitmapDrawable).bitmap, 100))
 
-        image.setColorFilter(resource.getColor(android.R.color.black)
-            ?: Color.parseColor("#000000"))
+        image.setColorFilter(itemView.context.resources.getColor(android.R.color.black))
 
         card.setOnClickListener {
             listener?.onSelected(drawableInfo)
