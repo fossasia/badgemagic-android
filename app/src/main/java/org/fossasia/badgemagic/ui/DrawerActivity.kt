398be7953373a609a67a5242c1a826fe8706d7ee
Tanmay Rauth <tanmayrauth@gmail.com>
2019-12-14 20:14:43 +0530

    feat: share app details on social platforms (#534)

--- app/src/main/java/org/fossasia/badgemagic/ui/DrawerActivity.kt
+++ app/src/main/java/org/fossasia/badgemagic/ui/DrawerActivity.kt
@@ -141,6 +141,13 @@ class DrawerActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedLi
                         R.id.buy -> {
                             startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://sg.pslab.io")))
                         }
+                        R.id.share_app_details -> {
+                            val shareIntent = Intent()
+                            shareIntent.type = "text/plain"
+                            shareIntent.action = Intent.ACTION_SEND
+                            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_msg))
+                            startActivity(Intent.createChooser(shareIntent, getString(R.string.share_using)))
+                        }
                         R.id.about -> {
                             switchFragment(AboutFragment.newInstance())
                             showMenu?.setGroupVisible(R.id.saved_group, false)
