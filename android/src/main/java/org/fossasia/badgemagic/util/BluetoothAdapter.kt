87e46e9cec3547a7cc7d720630a8139be20000d9
Dien-Nhung Nguyen-Phu <kein@kienlab.com>
2024-05-24 15:44:05 +0700

    fix: remove completely location permissions
    This fix is to satisfy Google, had been tested on Android 12 only.

--- android/src/main/java/org/fossasia/badgemagic/util/BluetoothAdapter.kt
+++ android/src/main/java/org/fossasia/badgemagic/util/BluetoothAdapter.kt
@@ -3,9 +3,6 @@ package org.fossasia.badgemagic.util
 import android.app.AlertDialog
 import android.bluetooth.BluetoothManager
 import android.content.Context
-import android.content.Intent
-import android.location.LocationManager
-import android.provider.Settings
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import org.fossasia.badgemagic.R
@@ -15,7 +12,7 @@ class BluetoothAdapter(appContext: Context) {
     private val btAdapter = btManager.adapter!!
 
     fun isTurnedOn(context: Context): Boolean = when {
-        btAdapter.isEnabled -> scanLocationPermissions(context)
+        btAdapter.isEnabled -> true
         else -> {
             showAlertDialog(context)
             false
@@ -26,22 +23,6 @@ class BluetoothAdapter(appContext: Context) {
         if (btAdapter.disable()) {
             btAdapter.enable()
         }
-    }
-
-    private fun scanLocationPermissions(context: Context): Boolean {
-        val lm = context.getSystemService(Context.LOCATION_SERVICE)
-        if (lm is LocationManager) {
-            if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-                AlertDialog.Builder(context)
-                    .setMessage(R.string.no_gps_enabled)
-                    .setPositiveButton("OK") { _, _ -> context.startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)) }
-                    .setNegativeButton("Cancel", null)
-                    .show()
-                return false
-            }
-            return true
-        }
-        return false
     }
 
     private fun showAlertDialog(context: Context) {
