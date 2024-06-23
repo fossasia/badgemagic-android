c1dc2c554ec836bd780b89d4a113eb9ba31e6f99
Aditya Gupta <adityaofficialgupta@gmail.com>
2024-06-23 22:09:00 +0100

    chore: add support for automatic screenshots

---
+++ test_integration/utils.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+
+import 'package:flutter_test/flutter_test.dart';
+
+Future<void> pumpUntilFound(
+  WidgetTester tester,
+  Finder finder, {
+  Duration timeout = const Duration(seconds: 10),
+}) async {
+  bool timerDone = false;
+  final timer = Timer(timeout, () => timerDone = true);
+  while (timerDone != true) {
+    await tester.pump();
+
+    final found = tester.any(finder);
+    if (found) {
+      timerDone = true;
+    }
+  }
+  timer.cancel();
+}
