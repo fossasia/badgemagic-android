6b4ef6f394aed8fa8839fbab8102ef66b007f568
Jhalak Upadhyay <89909502+Jhalakupadhyay@users.noreply.github.com>
2024-10-12 16:51:17 +0530

    feat: Added the scroll down animation. (#1025)

--- lib/badge_animation/ani_down.dart
+++ lib/badge_animation/ani_down.dart
@@ -1,13 +1,12 @@
 import 'package:badgemagic/badge_animation/animation_abstract.dart';
 
-class UpAnimation extends BadgeAnimation {
+class DownAnimation extends BadgeAnimation {
   @override
   void processAnimation(int badgeHeight, int badgeWidth, int animationIndex,
       List<List<bool>> processGrid, List<List<bool>> canvas) {
     int newWidth = processGrid[0].length;
     int newHeight = processGrid.length;
-    int animationValue = animationIndex ~/ ((newWidth * 2 / badgeHeight));
-
+    int animationValue = animationIndex ~/ ((newWidth / badgeHeight));
     for (int i = 0; i < badgeHeight; i++) {
       for (int j = 0; j < badgeWidth; j++) {
         if (j < badgeWidth && i < badgeHeight) {
@@ -15,7 +14,7 @@ class UpAnimation extends BadgeAnimation {
               i < newHeight &&
               j >= 0 &&
               j < newWidth &&
-              processGrid[(i + animationValue + newHeight) % newHeight][j]);
+              processGrid[(i - animationValue + newHeight) % newHeight][j]);
 
           canvas[i][j] = upCondition;
         }
